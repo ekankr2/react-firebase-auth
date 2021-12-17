81e88b06ca4a0ab3b1d724ecde3f231e36e4d931
ekank <ekankr2@naver.com>
2021-12-18 04:02:09 +0900

    authActions

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import App from './App';
 import './firebase/config'
 import 'bulma/css/bulma.min.css'
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
